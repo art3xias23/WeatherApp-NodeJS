db27d50d3cb75562fed01e0f5bdc41af511c6419
Konstantin Milchev <konstantin.v.milchev@gmail.com>
2018-03-08 16:08:04 +0000

    Added error handling

--- app.js
+++ app.js
@@ -1,23 +1,44 @@
 ``
 const request = require('request');
-const weather = require('weather-js');
-var leedsWeather;
+const yargs = require('yargs');
+const query = require('querystring');
+const argv = yargs
+    .options({
+      a:{
+        demand: true,
+        alias: 'address',
+        describe: 'Address to fetch weather for',
+        string: true
+      },
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
 
-weather.find({search: 'Leeds, UK', degreeType: 'C'}, function(err, result) {if(err) console.log(err);
-leedsWeather = JSON.stringify(result, null, 2);
-})
+console.log(encodeURIComponent(argv.a));
+var userSearchLocation = encodeURIComponent(argv.a);
 
 request({
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lambard%20street%20Philadelphia',
+  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${userSearchLocation}`,
   json: true
 },
-
   (error, response, body) => {
+if (error)
+{
+console.log('Unable to connect to Google servers');
+}
+else if(body.status === 'ZERO_RESULTS')
+{
+  console.log('No Results were returned');
+}
+else if(body.status==='OK')
+{
     var location = body.results[0].geometry.location;
-  console.log(`Address: ${JSON.stringify(body.results[0].geometry.location,undefined , 2)}`);
+
+      console.log(`Address: ${JSON.stringify(location,undefined , 2)}`);
   console.log(`Latitude: ${location.lat}`);
   console.log(`Longitude: ${location.lng}`);
-  console.log(`Leeds: ${leedsWeather}`);
+}
 
 // console.log(JSON.stringify(body, undefined, 2));
 });
